26fc547a1f84fe58b6e36627a337d170263615ab
Ron Coleman <ronncoleman@gmail.com>
2015-03-22 14:36:42 -0400

    Refactored.

--- Pretty/src/pretty/Analyze.scala
+++ Pretty/src/pretty/Analyze.scala
@@ -7,20 +7,17 @@ import fractop.fdresult
 
 object Analyze {
   def main(args: Array[String]): Unit = {
-    val fdr = new fdresult(true)
+    // Box value were taken from the GUI implementation.
     val boxes = Array(2, 3, 4, 6, 8, 12, 16, 32, 64, 128)
 
+    // Parameters, -10 (start q), 10 (end q), and 1 (increment) used only
+    // for multifractal analysis which is false
     BoxCountingMethod.setBoxOptions(boxes, false, -10, 10, 1)
     
-    val lines = Source.fromFile(args(0)).getLines().toList
-
-    val width = lines.foldLeft(0) { (width, line) =>
-      val len = line.length
-      if (len > width) len else width
-    }
+    val fdi = FdImage.getInstance(args(0))
     
-    val fdi = new FdImage(lines,width)
-
+    val fdr = new fdresult(true)
+    
     BoxCountingMethod.BoxCountingMethod(fdr,fdi)
   }
 
--- Pretty/src/util/FdImage.scala
+++ Pretty/src/util/FdImage.scala
@@ -3,30 +3,46 @@ package util
 import java.awt.Font
 import java.awt.Color
 import fractop.fdimage
+import scala.io.Source
 
 object FdImage {
   val POINT_SIZE = 10
   val LINE_HEIGHT = POINT_SIZE
+  
+  def getInstance(fileName: String): FdImage = {
+        val lines = Source.fromFile(fileName).getLines().toList
+
+    val width = lines.foldLeft(0) { (width, line) =>
+      val len = line.length
+      if (len > width) len else width
+    }
+    
+    val fdi = new FdImage(lines,width)
+    
+    fdi
+  }
 }
 
-class FdImage(lines: List[String], width: Int) extends 
-  fdimage(width*FdImage.POINT_SIZE,lines.length*FdImage.LINE_HEIGHT,true) {
-  
+class FdImage(lines: List[String], width: Int) extends fdimage(width * FdImage.POINT_SIZE, lines.length * FdImage.LINE_HEIGHT, true) {
   val g = createGraphics
-    g.setColor(Color.white)
-    g.fillRect(0, 0, width * FdImage.POINT_SIZE, lines.length * FdImage.LINE_HEIGHT)
-    println("w = " + width * FdImage.POINT_SIZE + " h = " + lines.length * FdImage.LINE_HEIGHT)
-    g.setColor(Color.black);
-    
-    g.setFont(new Font("Courier New", Font.PLAIN, FdImage.POINT_SIZE))
-    
-    lines.foldLeft(0) { (y, line) =>
-      line.foldLeft(0) { (x, c) =>
-        if(c != ' ')
-          g.fillRect(x, y, FdImage.POINT_SIZE-2, FdImage.POINT_SIZE)
-          
-        x + FdImage.POINT_SIZE
-      }
-      y + FdImage.LINE_HEIGHT
+  
+  g.setColor(Color.white)
+  
+  g.fillRect(0, 0, width * FdImage.POINT_SIZE, lines.length * FdImage.LINE_HEIGHT)
+  
+  println("w = " + width * FdImage.POINT_SIZE + " h = " + lines.length * FdImage.LINE_HEIGHT)
+  
+  g.setColor(Color.black)
+
+  g.setFont(new Font("Courier New", Font.PLAIN, FdImage.POINT_SIZE))
+
+  lines.foldLeft(0) { (y, line) =>
+    line.foldLeft(0) { (x, c) =>
+      if (c != ' ')
+        g.fillRect(x, y, FdImage.POINT_SIZE - 2, FdImage.POINT_SIZE)
+
+      x + FdImage.POINT_SIZE
     }
+    y + FdImage.LINE_HEIGHT
+  }
 }
