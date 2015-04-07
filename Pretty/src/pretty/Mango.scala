8f435c8616686b3f6341d47c600f427b9a57f8fb
Ron Coleman <ronncoleman@gmail.com>
2015-04-07 14:58:53 -0400

    Completed decommenting.

--- Pretty/src/pretty/Mango.scala
+++ Pretty/src/pretty/Mango.scala
@@ -1,11 +1,14 @@
 package pretty
 
 import java.util.regex.Pattern
+import scala.collection.mutable.HashMap
+import scala.io.Source
 
-object Symbols {
+object Mango {
   val keywords = List(
       "auto",
       "break",
+      "bool",
       "case",
       "char",
       "const",
@@ -36,6 +39,8 @@ object Symbols {
       "void",
       "volatile",
       "while",
+      "true",
+      "false",
       // Compiler directives
       "define",
       "include",
@@ -49,79 +54,134 @@ object Symbols {
       "pragma"
   )
   
-  val delims = "[ \\[*-+\\|/\\?\\.><=%\\$#!\\^&)(,;]"
+  val miscwords = List(
+      "NULL",
+      "size_t",
+      "printf"
+  )
+  
+  val delims = "[ \\}\\{\\]\\[*-+\\|/\\?\\.><=%\\$#!\\^&)(,:;]"
 
   def main(args:Array[String]): Unit = {
     val s = "static char *make_link_name (char const *name, char const *linkname);"
     
-    val words = s.split(delims)
-    words.foreach { word =>
-      if(word.length > 0) {
-        keywords.find(p => word == p) match {
-          case Some(s) =>
-          case None =>
-            println(word)
-        }
-      }
-    }
+//    val words = s.split(delims)
+//    words.foreach { word =>
+//      if(word.length > 0) {
+//        keywords.find(p => word == p) match {
+//          case Some(s) =>
+//          case None =>
+//            println(word)
+//        }
+//      }
+//    }
     
-    val line1 = "/* ABC */ 123 /* def";
+    val line1 = "/* ABC */ 123 /* def"
     val line2 = "xyz*/ hello world"
-    val w = decommentBlock(List(line1,line2)).foreach(p => println(p))
+    val line3 = "xyz*/"
+    val words = line3.split("\\*/")
+//    val w = filterBlockComments(List(line1,line2)).foreach(p => println(p))
     
 //    val line3 = "hello // world"
 //    decommentLine(List(line3)).foreach(p => println(p))
+    val lines = Source.fromFile("data/ls.c").getLines().toList
+//    val lines = List("#include <config.h>","#include <stdio.h>","#include <getopt.h>","#include <sys/types.h>")
     
+    val nocomments = filterBlockComments(lines).filter(p => p.trim().length >= 3)
+//    println(nocomments)
+    
+    val symtab = buildSymbolTable(nocomments).filter(t => t._1.trim().length >= 3)
+    println("viable symbols to mango: "+symtab.length)
+    println(symtab)
+    
+  }
+  
+ 
+
+  def buildSymbolTable(lines: List[String]): List[(String,Int)] = {
+    val symbolTable = new HashMap[String,Int]().withDefaultValue(0)
+    
+    lines.foreach { line =>
+      val words = line.split(delims+"+")
+      words.foreach { word =>
+        val cleanWord = clean(word)
+        if (word.length > 0) {
+          keywords.find(p => cleanWord == p) match {
+            case Some(s) =>
+            case None =>
+              symbolTable(cleanWord) += 1
+          }
+        }
+      }
+    }
+    symbolTable.toList.sortWith(_._2 > _._2)
+  }
+  
+  def clean(s: String): String = {
+    s.trim.replace("\"","")
+  }
+
+  /** Removes strings */
+  def filterStrings(lines: List[String]): List[String] = {
+    lines.foldLeft(List[String]()) { (composite, line) =>
+      if (line.count(p => p == '"') > 2)
+        composite
+      else {
+        val startString = line.indexOf("\"")
+        val endString = line.lastIndexOf("\"")
+        if (startString == -1 || startString == endString)
+          composite ++ List(line)
+        else {
+          composite ++ List(line.substring(0, startString) + line.substring(endString + 1))
+        }
+      }
+    }
   }
 
   /** Removes line comments */
-  def decommentLine(lines: List[String]): List[String] = {
+  def filterLineComments(lines: List[String]): List[String] = {
     lines.foldLeft(List[String]()) { (composite,line) =>
-      val quoteOpenIndex = line.indexOf("\"")
-      val quoteCloseIndex = if(quoteOpenIndex == -1) -1 else line.substring(quoteOpenIndex+1).indexOf("\"")
-
       val startCommentIndex = line.indexOf("//")
-      if(startCommentIndex == -1 || (startCommentIndex > quoteOpenIndex && startCommentIndex < quoteCloseIndex))
+      if(startCommentIndex == -1)
         composite ++ List(line)
       else
-        composite ++ List(line.substring(0,startCommentIndex))
+        composite ++ List(line.substring(0,startCommentIndex-1))
     }
   }
-  
+
   /** Removes block comments */
-  def decommentBlock(lines: List[String]): List[String] = {
-    println("INPUT")
-    lines.foreach(p => println(p))
-    println("---")
+  def filterBlockComments(lines: List[String]): List[String] = {
     val OPENING_BLOCK = "/\\*"
     val CLOSING_BLOCK = "\\*/"
     val s = lines.foldLeft((false, List[String]())) { (inCommentLines, line) =>
       val inComment = inCommentLines._1
       val lines = inCommentLines._2
+//      println(">>" + line)
 
       val openings = line.split(OPENING_BLOCK)
-//      println(openings.length)
-//      (0 until openings.length).foreach { n =>
-//        println(n + " : " + openings(n))
-//      }
-//      println("---")
+
       val startComment = line.contains("/*")
-      val u = openings.foldLeft((startComment, "")) { (inCommentLine, words) =>
-        val closings = words.split(CLOSING_BLOCK)
-        val endComment = words.contains("*/")
-        println("words:"+words+"||END COMMENT:"+endComment)
-        val v = (0 until closings.length).foldLeft(inCommentLine._2) { (comments, n) =>
-          if (n == 1)
-            comments + closings(n)
-          else
-            comments
+      val endComment = line.contains("*/")
+      val u = openings.foldLeft((startComment || inComment, "")) { (inCommentLine, words) =>
+        val inComment = inCommentLine._1
+        val line = inCommentLine._2
+        val closings = (words + " ").split(CLOSING_BLOCK)
+        if (closings.length == 1 && !inComment)
+          (inComment, words)
+        else {
+          val v = (0 until closings.length).foldLeft(line) { (comments, n) =>
+            if (n == 1)
+              comments + closings(n)
+            else
+              comments
+          }
+//          println((!endComment) + " || line:" + v)
+          (inComment && !endComment, v)
         }
-        println("in comment:"+(!endComment)+" line:"+v)
-        (!endComment||startComment, v)
       }
-      (u._1,lines ++ List(u._2))
+      (u._1, lines ++ List(u._2))
     }
-    if(s._1 != false)
+    if (s._1 != false)
       println("WARNING detected possible unclosed block comment.")
     s._2
   }
