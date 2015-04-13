4bd4e20dec29b04b9c48ee6b80e7fde0dcc9dab2
Ron Coleman <ronncoleman@gmail.com>
2015-04-13 08:57:56 -0400

    Added filter for strings and tabs.

--- Pretty/src/pretty/Mango.scala
+++ Pretty/src/pretty/Mango.scala
@@ -52,56 +52,64 @@ object Mango {
       "warning",
       "line",
       "pragma",
-      // Miscellaneous
+      // C-library
       "NULL",
       "size_t",
-      "printf"
+      "printf",
+      "sprintf",
+      "fprintf",
+      "open",
+      "close",
+      "fopen",
+      "fclose",
+      "fputs",
+      "fgets",
+      "stdout",
+      "stdin",
+      "stderr",
+      "free",
+      "malloc",
+      "main",
+      "argc",
+      "argv"
   )
   
-  val delims = "[ \\}\\{\\]\\[*-+\\|/\\?\\.><=%\\$#!\\^&)(,:;]"
+  val delims = "[ \\\\}\\{\\]\\[*\\-+\\|/\\?\\.><=%\\$#!\\^&)(,:;]"
+  
+  val TAB_STOP = 4
 
-  def main(args:Array[String]): Unit = {
-    val s = "static char *make_link_name (char const *name, char const *linkname);"
-    
-//    val words = s.split(delims)
-//    words.foreach { word =>
-//      if(word.length > 0) {
-//        keywords.find(p => word == p) match {
-//          case Some(s) =>
-//          case None =>
-//            println(word)
-//        }
-//      }
-//    }
-    
-
-//    val line3 = "xyz*/"
-//    val words = line3.split("\\*/")
-////    val w = filterBlockComments(List(line1,line2)).foreach(p => println(p))
+  def main(args:Array[String]): Unit = {  
+//    val t = s"#define MIN_COLUMN_WIDTH\t3"
+//    val t = s"ABCD\t\t3"
+//
+//    println(t.length)
 //    
-////    val line3 = "hello // world"
-////    decommentLine(List(line3)).foreach(p => println(p))
-//    val lines = Source.fromFile("data/ls.c").getLines().toList
-////    val lines = List("#include <config.h>","#include <stdio.h>","#include <getopt.h>","#include <sys/types.h>")
-//    
-//    val nocomments = filterBlockComments(lines).filter(p => p.trim().length >= 3)
-////    println(nocomments)
-//    
-//    val symtab = buildSymbolTable(nocomments).filter(t => t._1.trim().length >= 3)
-//    println("viable symbols to mango: "+symtab.length)
-//    println(symtab)
+//    val ls = filterTabs(List(t))
+//    println("12345678901234567890")
+//    println(ls(0))
     
+    val lines = filterStrings(filterTabs(Source.fromFile("data/cat.c").getLines().toList))
+       
+    // Get only symbols of a given length
+    val nocomments = filterComments(lines).filter(p => p.trim().length >= 3)
+    
+    // Get only symbols of a given frequency or higher
+    val symtab = buildSymbolTable(nocomments).filter(t => t._1.trim().length >= 3).sortWith(_._1.length > _._1.length)
+    
+    println("viable symbols to mango: "+symtab.length)
+    symtab.foreach { p =>
+      println("%3d %s".format(p._2,p._1))
+    }   
   }
   
- 
-
+  /** Returns the symbol table as a list of 2-tuples, (symbol,frequency). */
   def buildSymbolTable(lines: List[String]): List[(String,Int)] = {
     val symbolTable = new HashMap[String,Int]().withDefaultValue(0)
     
     lines.foreach { line =>
       val words = line.split(delims+"+")
       words.foreach { word =>
-        val cleanWord = clean(word)
+        val cleanWord = word.trim.replace("\"","")
         if (word.length > 0) {
           keywords.find(p => cleanWord == p) match {
             case Some(s) =>
@@ -111,16 +119,34 @@ object Mango {
         }
       }
     }
-    symbolTable.toList.sortWith(_._2 > _._2)
+    symbolTable.toList
   }
   
-  def clean(s: String): String = {
-    s.trim.replace("\"","")
+  /** Returns tabs replaced with spaces. */
+  def filterTabs(lines: List[String]): List[String] = {
+    lines.map { line =>
+      val buffer = new StringBuffer("")
+      
+      line.foldLeft((1,"")) { (posNewLine,c) =>
+        val pos = posNewLine._1
+        val newLine = posNewLine._2
+        
+        val offset = c match {
+          case '\t' =>
+            // Pad is distance in spaces to next tab stop
+            val padAmt = TAB_STOP - (pos % TAB_STOP)
+            for(i <- 0 until padAmt) buffer.append(' ')
+            padAmt
+          case _ =>
+            buffer.append(c)
+            1
+        }
+        (pos+offset,buffer.toString())
+        } . _2
+    }
   }
-
-  /** Removes strings */
-
-  /** Removes line comments */
+  
+  /** Returns lines without line-oriented comments. */
   def filterLineComments(lines: List[String]): List[String] = {
     lines.foldLeft(List[String]()) { (composite,line) =>
       val startCommentIndex = line.indexOf("//")
@@ -131,43 +157,65 @@ object Mango {
     }
   }
 
-  
+  /** Returns lines without strings. */
   def filterStrings(lines: List[String]): List[String] = {
     // code "..." code
-    val SCENARIOA = """(.*)\".*\"(.*)""".r
+    val SCENARIO_A = """(.*)\".*\"(.*)""".r
     
     // code "...
-    val SCENARIOB = """(.*)\".*""".r
+    val SCENARIO_B = """(.*)\".*""".r
     
     // ..." code
-    val SCENARIOC = """.*\"(.*)""".r
-    null
+    val SCENARIO_C = """.*\"(.*)""".r
+    
+    val output = (0 until lines.length).foldLeft((false,List[String]())) { (stringLines,k) =>
+      if(k > 4568)
+        k
+        
+      val inComment = stringLines._1
+      
+      // Get rid of trim spaces and escaped quotes
+      val line = lines(k).trim .replaceAll("\\\\\"","@")
+      
+      line match {
+        case SCENARIO_A(codeBefore,codeAfter) =>
+          (false,stringLines._2 ++ List(codeBefore+"\""+"\""+codeAfter))
+          
+        case SCENARIO_B(code) if !inComment =>           
+          (true,stringLines._2 ++ List(code+"\""))
+          
+        case SCENARIO_C(code) if inComment =>            
+          (false,stringLines._2 ++ List("\""+code))
+          
+        case _ =>
+          if(inComment)
+            (true,stringLines._2)
+          else
+            (false,stringLines._2 ++ List(line))
+      }
+    }
+    
+    output._2
   }
-//  val line0 = """GOOG:310.84"""
-//    val line1 = "/* ABC */"
-//    val line2 = "xyz*/ hello world"
-//    filterBlockComments2(List(line1,line2)).foreach(p => println(p))  
-  val lines = Source.fromFile("data/ls.c").getLines().toList
-  filterBlockComments(lines).foreach(p => println(p))
   
-  def filterBlockComments(lines: List[String]): List[String] = {   
+  def filterComments(lines: List[String]): List[String] = {   
     // /* comment */ code
-    val SCENARIO0 = """/\*.*\*/(.*)""".r
+    val SCENARIO_A = """/\*.*\*/(.*)""".r
     
     // code /* comment
-    val SCENARIO1 = """(.*)/\*.*""".r
+    val SCENARIO_B = """(.*)/\*.*""".r
     
     // */ code
-    val SCENARIO2 = """.*\*/(.*)""".r
+    val SCENARIO_C = """.*\*/(.*)""".r
     
     // code /* comment */
-    val SCENARIO3 = """(.*)/\*.*\*/""".r
+    val SCENARIO_D = """(.*)/\*.*\*/""".r
     
     // code /* comment */ code
-    val SCENARIO4 = """(.*)/\*.*\*/(.*)""".r
+    val SCENARIO_E = """(.*)/\*.*\*/(.*)""".r
     
     // code //
-    val SCENARIO5 = """(.*)//.*""".r
+    val SCENARIO_F = """(.*)//.*""".r
     
     val output = (0 until lines.length).foldLeft((false,List[String]())) { (commentLines,k) =>
       val inComment = commentLines._1
@@ -178,25 +226,25 @@ object Mango {
 //      }
 
       line match {
-        case SCENARIO0(code) =>
+        case SCENARIO_A(code) =>
           (false,commentLines._2 ++ List(code))
           
-        case SCENARIO1(code) =>
+        case SCENARIO_B(code) =>
           (true,commentLines._2 ++ List(code))
           
-        case SCENARIO3(code) =>
+        case SCENARIO_D(code) =>
           (false,commentLines._2 ++ List(code))
           
-        case SCENARIO4(codeBefore,codeAfter) =>
+        case SCENARIO_E(codeBefore,codeAfter) =>
           (false,commentLines._2 ++ List(codeBefore,codeAfter))
           
-        case SCENARIO2(code) =>
+        case SCENARIO_C(code) =>
           if(!inComment)
-            println("warning detected possible bad block comment line "+k)
+            println("warning detected possible bad block comment line "+(k+1))
             
           (false,commentLines._2 ++ List(code))
 
-        case SCENARIO5(code) =>
+        case SCENARIO_F(code) =>
           (inComment,List(code) ++ commentLines._2)
           
         case _ =>
@@ -206,6 +254,7 @@ object Mango {
             (inComment, commentLines._2 ++ List(line))
       }
     }
+    
     if(output._1 != false)
       println("warning reached end of lines with open comment block.")
       
