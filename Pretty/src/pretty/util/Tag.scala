ca5830a87119f0739b4deb6fe1334ffb3a8419fd
Ron Coleman <ronncoleman@gmail.com>
2016-07-17 12:54:46 -0400

    Started auto-tag processing with cpp processing.

--- Pretty/src/pretty/util/Tag.scala
+++ Pretty/src/pretty/util/Tag.scala
@@ -1,24 +1,62 @@
 package pretty.util
 
 import java.io.FileInputStream
-
 import org.antlr.v4.runtime.ANTLRInputStream
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.ParserRuleContext
 import org.antlr.v4.runtime.tree.ErrorNode
 import org.antlr.v4.runtime.tree.ParseTreeListener
 import org.antlr.v4.runtime.tree.TerminalNode
-
 import cgrammar.CLexer
 import cgrammar.CParser
 import pretty.readability.HalsteadParserListener
+import java.nio.file.Files
+import java.io.File
+import scala.util.Random
+import scala.collection.mutable.ListBuffer
+import java.io.ByteArrayInputStream
+import scala.io.Source
+import scala.collection.mutable.HashMap
 
 object Tag {
-  def main(args: Array[String]): Unit = {
-    val in = new FileInputStream(args(0))
     
-    // Get tne ANTLR parser loaded
-    val input = new ANTLRInputStream(in)
+  val ran = new Random(0)
+  
+  def main(args: Array[String]): Unit = {    
+    
+    FileWalker.walk(args(0), ".c", action)
+    
+  }
+  
+  def action(file: File): Unit = {
+
+//    // Filter out compiler directives since C.g4 cant handle them
+//    val in = new FileInputStream(file)
+//    
+//    import org.apache.commons.io.IOUtils
+//    val bytes = IOUtils.toByteArray(in)
+//    
+//    val path = file.getAbsolutePath
+//    println(">>>>> "+path)
+//
+//    val filteredlines = Source.fromBytes(bytes).getLines.toList.filter { line =>
+//      val pass = !line.startsWith("#")
+//      
+//      if(!pass)
+//        println(line)
+//        
+//      pass
+//    }.foldLeft("") { (accum, line) => accum + line + "\n" }
+//    
+//    println("----- "+filteredlines.length+" lines remain")
+//    println(filteredlines)
+//    
+//    val filteredBytes = filteredlines.getBytes
+//    
+//    val bis = new ByteArrayInputStream(filteredBytes)
+
+    // Compile the file looking for methods
+    val input = new ANTLRInputStream(strip(file))
 
     val lexer = new CLexer(input)
     
@@ -31,15 +69,78 @@ object Tag {
     parser.addParseListener(listener)
     
     parser.compilationUnit
+    
+    print(file.getAbsolutePath+ " ")
+    
+    listener.marks.foreach { mark =>
+      print(mark + " ")
+    }
+    println
+  }
+  
+  def strip(file: File): ByteArrayInputStream = {
+    // Filter out compiler directives since C.g4 cant handle them
+    val in = new FileInputStream(file)
+    
+    import org.apache.commons.io.IOUtils
+    val bytes = IOUtils.toByteArray(in)
+    
+    val path = file.getAbsolutePath
+    println(">>>>> "+path)
+
+    val lines = Source.fromBytes(bytes).getLines.toList
+     
+    val state = (0 until lines.length).foldLeft((HashMap[Int,Int](),false)) { (state, k) =>
+      val (skipLineNums, inhash) = state
+      
+      val line = lines(k).trim
+      
+      if(line.startsWith("#") && line.endsWith("\\")) {
+        skipLineNums(k) = k
+        skipLineNums(k+1) = k+1
+        (skipLineNums, true)
+      }
+      else if(line.startsWith("#")) {
+        skipLineNums(k) = k
+        (skipLineNums, false)
+      }
+      else if(line.endsWith("\\") && inhash) {
+        skipLineNums(k+1) = k+1
+        (skipLineNums, true)
+      }
+      else {
+        (skipLineNums, false)
+      }
+    }
+    
+    // Remove the lines by commenting them out
+    val (skipLineNums, _) = state
+    
+    val filteredLinesAsString = (0 until lines.length).foldLeft("") { (accum, k) =>
+      if(skipLineNums.contains(k)) 
+        accum + "//" + lines(k) + "\n"
+      else
+        accum + lines(k) + "\n"
+    }
+
+    println(filteredLinesAsString)
+    
+    in.close
+    
+    val bis = new ByteArrayInputStream(filteredLinesAsString.getBytes)
+    
+    bis
   }
 }
 
 class TagParserListener(tokenStream: CommonTokenStream) extends ParseTreeListener   {
+  val marks = ListBuffer[Int]()
+  
   def enterEveryRule(arg: ParserRuleContext): Unit = {  
     arg match {
       case node: CParser.FunctionDefinitionContext =>
         val lineno = node.getStart.getLine
-        println(lineno)
+        marks.append(lineno)
         
       case _ =>
     }
